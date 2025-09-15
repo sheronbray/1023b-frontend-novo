d1714ecd3979a307ec136f45ea079dfbac71b24f
sheronbray <sheron.santos@estudante.ifms.edu.br>
2025-09-15 08:27:40 -0400

    configurando variaveis de ambiente para o backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type ProdutoType = {
+  _id: string;
+  nome: string;
+  preco: number;
+  urlfoto:string;
+  descricao: string;
+}
 
-  return (
+function App() {
+  const [produtos, setProdutos] = useState<ProdutoType[]>([]);
+  useEffect(() => {
+    fetch('/api/produtos')
+      .then(response => response.json())
+      .then(data => setProdutos(data));
+  }, []);
+
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    <div>
+      {produtos.map(produto => (
+        <div key={produto._id}>
+          <h2>{produto.nome}</h2>
+          <p>Preço: {produto.preco}</p>
+          <img src={produto.urlfoto} alt={produto.nome} />
+          <p>{produto.descricao}</p>
+        </div>
+      ))}
+    </div>
     </>
   )
 }
 
 export default App
+
