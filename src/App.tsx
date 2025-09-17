354cd73b40d19a5dbf2c58ccf4d9892be28430a6
sheronbray <sheron.santos@estudante.ifms.edu.br>
2025-09-17 11:50:16 -0400

    front end do post para adicionar produtos

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,23 @@ type ProdutoType = {
 
 function App() {
   const [produtos, setProdutos] = useState<ProdutoType[]>([]);
+  const handleForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+    data.preco = parseFloat(data.preco as string);
+    fetch('/api/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(newProduto => setProdutos([...produtos, newProduto]));
+    form.reset();
+  }
   useEffect(() => {
     fetch('/api/produtos')
       .then(response => response.json())
@@ -19,8 +36,16 @@ function App() {
 
   return(
     <>
-    
-    <div>
+    <div>Cadastro de Produtos<div/>
+    <form onSubmit={handleForm}>
+      <input type="text" name="nome" placeholder="Nome" />
+      <input type="number" name="preco" placeholder="Preço" />
+      <input type="text" name="urlfoto" placeholder="URL da Foto" />
+      <input type="text" name="descricao" placeholder="Descrição" />
+      <button type="submit">Adicionar Produto</button>
+    </form>
+    <div>Lista de produtos</div>
+
       {produtos.map(produto => (
         <div key={produto._id}>
           <h2>{produto.nome}</h2>
